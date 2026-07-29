aefb61010b850e4c7414d894f581198e62f9dc59
chief_andrew <chief.andrew5891@gmail.com>
2026-07-29 21:35:48 +0200

    commit 29/07/2026 21:35

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -52,5 +52,6 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:33.9.0"))
     implementation("com.google.firebase:firebase-firestore")
     implementation("com.google.firebase:firebase-auth:22.3.1")
+
     implementation("androidx.compose.material:material-icons-extended")
 }
