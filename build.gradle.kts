d42af6c39fb1346b826c19fe1163d29b92ffedb1
chief_andrew <chief.andrew5891@gmail.com>
2026-08-02 18:37:06 +0200

    commit 02/08/2026 18:36

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,5 +3,7 @@ plugins {
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.multiplatform) apply false
+    alias(libs.plugins.compose.compiler) apply false
+    alias(libs.plugins.compose.multiplatform) apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,4 +15,5 @@ dependencyResolutionManagement {
 
 rootProject.name = "BeautyPlannerClient"
 include(":shared")
+include(":composeApp")
 include(":androidApp")
